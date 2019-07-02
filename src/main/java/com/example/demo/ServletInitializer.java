b87cbc3ca78caa4e0619558d21ee2b83804b6fb7
kakankari12345 <kalankari12345@gmail.com>
2019-07-02 17:38:00 +0530

    sample method 2 is added

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(GtiProjectApplication.class);
+	}
+	public static void sample2() {
+		System.out.println("welcome github project");
 	}
 
 }
