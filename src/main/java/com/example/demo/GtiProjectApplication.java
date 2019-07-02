17956de573457ae05facb19cc31f79e83dbd008d
kakankari12345 <kalankari12345@gmail.com>
2019-07-02 17:03:05 +0530

    gitProject is added

---
+++ src/main/java/com/example/demo/GtiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtiProjectApplication.class, args);
+	}
+
+}
